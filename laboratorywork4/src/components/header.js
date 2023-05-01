f1d3e787d4ca7b98f6c5358d8942bcc66a800e11
dann1kk <daniel.pogorevici@isa.utm.md>
2023-05-01 14:52:50 +0300

    fix header bug

--- laboratorywork4/src/components/header.js
+++ laboratorywork4/src/components/header.js
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 function Header() {
   const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('user-info'));
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('user-info')));
+  const location = useLocation();
 
+  
   const logout = () => {
     localStorage.removeItem('user-info');
     setIsLoggedIn(false);
     setUser(null);
     window.location.href = '/login'; 
   };
+
+  useEffect(() => {
+    setIsLoggedIn(!!localStorage.getItem('user-info'));
+    setUser(JSON.parse(localStorage.getItem('user-info')));
+    if (location.pathname === '/main') {
+      setIsLoggedIn(true);
+    }
+  }, [isLoggedIn, location.pathname]); 
 
   useEffect(() => {
     setIsLoggedIn(!!localStorage.getItem('user-info'));
